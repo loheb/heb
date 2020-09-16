ac70b6a51e7ac3fbe965d4e2738421b6f6d56cd2
loheb <35033926+loheb@users.noreply.github.com>
2020-09-16 02:08:18 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,8 +4,6 @@ from discord.ext.commands import has_permissions
 
 bot = commands.Bot(command_prefix='!', case_insensitive=True)
 
-discord_token = ('NzU0MzI1NDY1MDIzNjQzNzU5.X1zGVQ.9YBtexEWN0Yrk-CjJN0nPyPsbsY')
-
 @bot.command()
 @has_permissions(administrator=True)
 async def load(ctx, extension):
@@ -18,8 +16,8 @@ async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     await ctx.send('Command has been unloaded.')
 
-for filename in os.listdir(path=r'C:\Users\theaw\PycharmProjects\heb-bot\code\cogs'):
+for filename in os.listdir(path='.\code\cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run(discord_token)
+bot.run(process.env((TOKEN))
